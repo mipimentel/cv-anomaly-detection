b90390aeed222f34d00a48159a0752b39c6ccc2f
Marcelo Pimentel <marcelo.ide.pimentel@gmail.com>
2023-08-30 23:10:34 -0300

    test: add unit tests for image metrics

---
+++ tests/metrics/test_img_metrics.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pytest
+
+from cv_anomaly_detection.metrics.img_metrics import measure_brightness
+
+
+def test_measure_brightness():
+    """Test measure_brightness."""
+
+    bright = np.ones((100, 100, 3), dtype=np.uint8) * 255
+    bright_brightness = measure_brightness(bright.astype(np.uint8))
+
+    assert bright_brightness == 255
+
+    dark = np.zeros((100, 100, 3), dtype=np.uint8)
+    dark_brightness = measure_brightness(dark)
+
+    assert dark_brightness == 0
+
+    img_rnd = np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)
+    rnd_brightness = measure_brightness(img_rnd)
+
+    assert isinstance(rnd_brightness, float)
+    assert 255 > rnd_brightness > 0
