bafaea518aca0bd8454090dd8844569c161c5fe8
Marcelo Ide Pimentel <marcelo.ide.pimentel@gmail.com>
2024-01-14 20:43:15 -0300

    add translation to portuguese

--- cv_anomaly_detection/utils/__init__.py
+++ cv_anomaly_detection/utils/__init__.py
@@ -5,3 +5,19 @@ BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__
 DATA_DIR = os.path.join(BASE_DIR, "datasets")
 MVTEC_AD = os.path.join(DATA_DIR, "mvtec")
 # MODELS_DIR = os.path.join(BASE_DIR, 'models')
+
+# translation dict for portuguese plots
+# df["class"] = df["class"].map(translation_dict)
+translation_dict = {
+    'good': 'conforme',
+    "bent_wire": 'fio dobrado',
+    'missing_wire': 'fios faltantes',
+    'missing_cable': 'cabo faltante',
+    'cut_inner_insulation': 'isolamento interno cortado',
+    'cut_outer_insulation': 'isolamento externo cortado',
+    'poke_insulation': 'isolamento furado',
+    "cable_swap": 'cabo trocado',
+    'combined': "defeitos mistos",
+}
+
+
